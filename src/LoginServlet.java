3b4aabad313634a9aa40feb6d3ca924c7014c03e
redpig <lihuang_man@hotmail.com>
2019-03-18 23:34:15 +0800

    update LoginServlet.java

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -54,6 +54,16 @@ public class LoginServlet extends HttpServlet {
         String name = request.getParameter("name");
         String password = request.getParameter("password");
 
+        System.out.println("浏览器发出请求时的完整URL，包括协议 主机名 端口(如果有): " + request.getRequestURL());
+        System.out.println("浏览器发出请求的资源名部分，去掉了协议和主机名: " + request.getRequestURI());
+        System.out.println("请求行中的参数部分: " + request.getQueryString());
+        System.out.println("浏览器所处于的客户机的IP地址: " + request.getRemoteAddr());
+        System.out.println("浏览器所处于的客户机的主机名: " + request.getRemoteHost());
+        System.out.println("浏览器所处于的客户机使用的网络端口: " + request.getRemotePort());
+        System.out.println("服务器的IP地址: " + request.getLocalAddr());
+        System.out.println("服务器的主机名: " + request.getLocalName());
+        System.out.println("得到客户机请求方式: " + request.getMethod());
+
         if ("admin".equals(name) && "123".equals(password)) {
             request.getRequestDispatcher("success.html").forward(request, response);
         } else {
